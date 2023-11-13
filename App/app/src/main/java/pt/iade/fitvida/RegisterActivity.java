e2fbdc071caba1d4dbd28cc7dd70394cf796df80
Mario-Igreja <123427601+Mario-Igreja@users.noreply.github.com>
2023-11-13 04:45:17 +0000

    Updating activiyties and applied view-jumps.

--- App/app/src/main/java/pt/iade/fitvida/RegisterActivity.java
+++ App/app/src/main/java/pt/iade/fitvida/RegisterActivity.java
@@ -2,7 +2,9 @@ package pt.iade.fitvida;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void startHome(View view){
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
     }
 }
